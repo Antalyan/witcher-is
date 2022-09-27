6c954c25784b9f375d25110c7f07c6613a0fcdaa
Michael Koudela <xkoudel2@fi.muni.cz>
2022-09-27 19:46:50 +0200

    Update ignore and init data

--- KaerMorhenIS/Data/PersonInitializer.cs
+++ KaerMorhenIS/Data/PersonInitializer.cs
@@ -7,12 +7,7 @@ public static class PersonInitializer
 {
     public static void Seed(this ModelBuilder modelBuilder)
     {
-        var jozko = new Person
-        {
-            Id = 1, Name = "Jozko", Surname = "Mrkvicka", Cv = "hrotic", Login = "makac", PasswordHash = "1111",
-            IsActive = true, Birthdate = DateTime.Now
-        };
-
-        modelBuilder.Entity<Person>().HasData(jozko);
+        modelBuilder.Entity<Person>().HasData(new Person {Name = "Jozko", Surname = "Mrkvicka", Cv = "hrotic", Login = "makac", PasswordHash = "1111",
+            IsActive = true, Birthdate = DateTime.Now});
     }
 }
