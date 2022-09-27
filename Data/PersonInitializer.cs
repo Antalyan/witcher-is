ef65f9c585ff955b625fd3e7016c883432e184d1
Michael Koudela <xkoudel2@fi.muni.cz>
2022-09-27 19:14:04 +0200

    Initial commit

---
+++ Data/PersonInitializer.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WitcherProject.Data.Models;
+
+namespace WitcherProject.Data;
+
+public static class PersonInitializer
+{
+    public static void Seed(this ModelBuilder modelBuilder)
+    {
+        var jozko = new Person
+        {
+            Id = 1, Name = "Jozko", Surname = "Mrkvicka", Cv = "hrotic", Login = "makac", PasswordHash = "1111",
+            IsActive = true, Birthdate = DateTime.Now
+        };
+
+        modelBuilder.Entity<Person>().HasData(jozko);
+    }
+}
---
+++ Migrations/20220927170119_Initial.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WitcherProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Persons",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Login = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PasswordHash = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Surname = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cv = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Birthdate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Persons", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Persons",
+                columns: new[] { "Id", "Birthdate", "Cv", "IsActive", "Login", "Name", "PasswordHash", "Surname" },
+                values: new object[] { 1, new DateTime(2022, 9, 27, 19, 1, 19, 33, DateTimeKind.Local).AddTicks(4828), "hrotic", true, "makac", "Jozko", "1111", "Mrkvicka" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Persons");
+        }
+    }
+}
