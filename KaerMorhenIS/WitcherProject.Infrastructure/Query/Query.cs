d4f1d33066c702f37f59db3f5db7a170c29db1e2
Michael Koudela <xkoudel2@fi.muni.cz>
2022-10-17 22:00:14 +0200

    Impl first version of query

--- KaerMorhenIS/WitcherProject.Infrastructure/Query/Query.cs
+++ KaerMorhenIS/WitcherProject.Infrastructure/Query/Query.cs
@@ -1,6 +1,31 @@
-﻿namespace WitcherProject.Infrastructure.Query;
+﻿using System.Linq.Expressions;
 
-public class Query
+namespace WitcherProject.Infrastructure.Query;
+
+public abstract class Query<TEntity>: IQuery<TEntity> where TEntity: class
 {
-    
+    protected IQueryable<TEntity> _query;
+    public abstract Task<IEnumerable<TEntity>> ExecuteAsync();
+
+    public Query<TEntity> Filter(Expression<Func<TEntity, bool>> condition)
+    {
+        _query = _query.Where(condition);
+        return this;
+    }
+
+    public Query<TEntity> OrderBy<TKey>(Expression<Func<TEntity, TKey>> condition, bool ascending = true)
+    {
+        _query = ascending switch
+        {
+            true => _query.OrderBy(condition),
+            false => _query.OrderByDescending(condition)
+        };
+        return this;
+    }
+
+    public Query<TEntity> Page(int page, int pageSize = 10)
+    {
+        _query = _query.Skip((page - 1) * pageSize).Take(pageSize);
+        return this;
+    }
 }
