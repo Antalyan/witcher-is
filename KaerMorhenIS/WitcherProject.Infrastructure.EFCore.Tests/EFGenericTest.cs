6c2d63704c28196a648d02affc6e569131697fbb
Michael Koudela <xkoudel2@fi.muni.cz>
2022-10-31 14:12:15 +0100

    Options init moved to common parent class

---
+++ KaerMorhenIS/WitcherProject.Infrastructure.EFCore.Tests/EFGenericTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.Extensions.DependencyInjection;
+using WitcherProject.DAL;
+
+namespace WitcherProject.Infrastructure.EFCore.Tests;
+
+public abstract class EFGenericTest
+{
+    protected DbContextOptions<KaerMorhenDBContext> _options;
+
+    protected EFGenericTest()
+    {
+        // based on https://github.com/Lukas-Razz/CSharp_Demo-QueryObject/blob/master/Demo.QueryObject.Infrastructure.EFCore.Tests/QueryObjectTests.cs
+
+        var serviceProvider = new ServiceCollection()
+            .AddEntityFrameworkInMemoryDatabase()
+            .BuildServiceProvider();
+
+        _options = new DbContextOptionsBuilder<KaerMorhenDBContext>()
+            .UseInMemoryDatabase($"test_db_{DateTime.Now.ToFileTimeUtc()}")
+            .UseInternalServiceProvider(serviceProvider)
+            .Options;
+        
+        
+        
+    }
+}
