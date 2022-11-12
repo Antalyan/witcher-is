038f591f036d93981b627784d252d7943de2509e
peter-sipos <peter.siipos@gmail.com>
2022-11-12 16:31:06 +0100

    Added BL file structure, added Mapster to BL, added the basic classes for person service without conrete implementation.

---
+++ KaerMorhenIS/WitcherProject.BL/DTOs/NewPersonDto.cs
@@ -0,0 +1,11 @@
+﻿namespace WitcherProject.BL.DTOs;
+
+public class NewPersonDto
+{
+    public string Login { get; set; }
+    public string Name { get; set; }
+    public string? Surname { get; set; }
+    public string? Cv { get; set; }
+    public DateTime Birthdate { get; set; }
+    public bool IsActive { get; set; }
+}
---
+++ KaerMorhenIS/WitcherProject.BL/Services/PersonService.cs
@@ -0,0 +1,8 @@
+﻿namespace WitcherProject.BL;
+
+public class PersonService
+{
+    //todo add new user
+    // todo assign role to user
+    // todo change personal data
+}
