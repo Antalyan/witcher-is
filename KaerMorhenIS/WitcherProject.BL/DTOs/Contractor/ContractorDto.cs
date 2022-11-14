4bbdf4f86a206a19a68861e085d0d6662c406626
Michael Koudela <xkoudel2@fi.muni.cz>
2022-11-14 11:56:02 +0100

    Impl contractor service

---
+++ KaerMorhenIS/WitcherProject.BL/DTOs/Contractor/ContractorDto.cs
@@ -0,0 +1,12 @@
+﻿namespace WitcherProject.BL.DTOs.Contractor;
+
+public class ContractorDto
+{
+    public int? Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string? Surname { get; set; }
+
+    // public virtual List<Contract> Contracts { get; set; } TODO: add contractDto list or id list or nothing?
+}
--- KaerMorhenIS/WitcherProject.BL/Services/IContractorService.cs
+++ KaerMorhenIS/WitcherProject.BL/Services/IContractorService.cs
@@ -1,6 +1,17 @@
-﻿namespace WitcherProject.BL.Services;
+﻿using WitcherProject.BL.DTOs.Contract;
+using WitcherProject.BL.DTOs.Contractor;
+
+namespace WitcherProject.BL.Services;
 
 public interface IContractorService
 {
-   
+    Task CreateContractorAsync(ContractorDto contractDto);
+
+    Task<IEnumerable<ContractorDto>> GetAllContractorsAsync();
+
+    Task<ContractorDto> GetContractorByIdAsync(int contractorId);
+
+    Task UpdateContractAsync(ContractorDto contractorDto);
+
+    Task DeleteContractorAsync(int contractId);
 }
