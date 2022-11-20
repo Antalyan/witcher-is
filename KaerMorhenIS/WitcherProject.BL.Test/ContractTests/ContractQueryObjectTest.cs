194580600debc1c18545c106fd7fd755f0597ff1
Patrik Procházka <xproch23@fi.muni.cz>
2022-11-20 17:33:01 +0100

    ContractQueryObjectTest refactor #17
    Signed-off-by: Patrik Procházka <xproch23@fi.muni.cz>

--- KaerMorhenIS/WitcherProject.BL.Test/ContractTests/ContractQueryObjectTest.cs
+++ KaerMorhenIS/WitcherProject.BL.Test/ContractTests/ContractQueryObjectTest.cs
@@ -1,4 +1,5 @@
-﻿using Moq;
+﻿using System.Linq.Expressions;
+using Moq;
 using WitcherProject.BL.DTOs.Contract;
 using WitcherProject.BL.QueryObjects;
 using WitcherProject.DAL.Models;
@@ -9,7 +10,6 @@ namespace WitcherProject.BL.Test.ContractTests;
 
 public class ContractQueryObjectTest : ContractServiceTest
 {
-    
     [Fact]
     public async Task ContractQueryObject_Filtered()
     {
@@ -18,23 +18,23 @@ public class ContractQueryObjectTest : ContractServiceTest
         var filter = new ContractFilterDto
         {
             Name = _beastOfHonorton.Name,
-            Deadline = _beastOfHonorton.Deadline
+            Deadline = _beastOfHonorton.Deadline,
+            SortAscending = false,
         };
-        
-        mockQuery.Setup(mcq => mcq.Filter(cont => !string.IsNullOrEmpty(filter.Name) && cont.Name == filter.Name)).Verifiable();
-        mockQuery.Setup(mcq => mcq.Filter(contract => filter.Deadline != null && contract.Deadline == filter.Deadline)).Verifiable();
+
+        mockQuery.Setup(mcq => mcq.Filter(It.IsAny<Expression<Func<Contract, bool>>>())).Verifiable();
+        mockQuery.Setup(mcq => mcq.OrderBy(It.IsAny<Expression<Func<Contract, int>>>(), false)).Verifiable();
         mockQuery.Setup(mcq => mcq.ExecuteAsync().Result)
-            .Returns(new List<Contract>{_beastOfHonorton});
+            .Returns(new List<Contract> { _beastOfHonorton });
 
         var contractQueryObject = new ContractQueryObject(mockQuery.Object);
 
         var result = await contractQueryObject.ExecuteQuery(filter);
-        
-        mockQuery.Verify(mcq => mcq.Filter(cont => !string.IsNullOrEmpty(filter.Name) && cont.Name == filter.Name), Times.Once);
-        
-        mockQuery.Verify(mcq => mcq.Filter(contract => filter.Deadline != null && contract.Deadline == filter.Deadline), Times.Once);
-        
-        Assert.Single(result);
 
+        mockQuery.Verify(mcq => mcq.Filter(It.IsAny<Expression<Func<Contract, bool>>>()), Times.AtLeast(2));
+
+        mockQuery.Verify(mcq => mcq.OrderBy(It.IsAny<Expression<Func<Contract, int>>>(), false), Times.Once);
+
+        Assert.Single(result);
     }
 }
