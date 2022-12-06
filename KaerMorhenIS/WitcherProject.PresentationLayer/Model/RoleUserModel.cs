75689e96dad874aa07a7133dfa882d17ddfce961
Patrik Procházka <xproch23@fi.muni.cz>
2022-12-06 21:48:26 +0100

    Role init with AdminPage #17
    Signed-off-by: Patrik Procházka <xproch23@fi.muni.cz>

---
+++ KaerMorhenIS/WitcherProject.PresentationLayer/Model/RoleUserModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Build.Framework;
+
+namespace WitcherProject.PresentationLayer.Model;
+
+public class RoleUserModel
+{
+    [Required]
+    public string UserName { get; set; }
+    
+    [Required]
+    public string RoleName { get; set; }
+}
---
+++ KaerMorhenIS/WitcherProject.Shared/RoleNames.cs
@@ -0,0 +1,11 @@
+﻿namespace WitcherProject.Shared.Enums;
+
+public static class RoleNames
+{
+    public const string ADMIN_ROLE_NAME = "Admin";
+
+    public const string MASTER_ROLE_NAME = "MASTER";
+    
+    public const string WITCHER_ROLE_NAME = "Witcher";
+
+}
