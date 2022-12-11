79e754726fda3b3b61f43704f99359d197343fc6
Patrik Procházka <xproch23@fi.muni.cz>
2022-12-11 14:19:10 +0100

    Data Initialization update - added roles - added new user

--- KaerMorhenIS/WitcherProject.DAL/Data/Initializers/DataInitializer.cs
+++ KaerMorhenIS/WitcherProject.DAL/Data/Initializers/DataInitializer.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System.Globalization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using WitcherProject.DAL.Models;
+using WitcherProject.Shared;
 using WitcherProject.Shared.Enums;
 
 namespace WitcherProject.DAL.Data.Initializers;
@@ -23,8 +25,39 @@ public static class DbInitializer
             SecurityStamp = Guid.NewGuid().ToString()
         };
         
-        geralt.PasswordHash = new PasswordHasher<Person>().HashPassword(geralt, "GeraltOfRevia123");
+        var vesemir = new Person
+        {
+            Id = 2,
+            Name = "Vesemir",
+            Surname = "",
+            Cv = "Old coot",
+            UserName = "vesemir",
+            IsActive = true,
+            Birthdate = DateTime.Now,
+            NormalizedUserName = "VESEMIR",
+            SecurityStamp = Guid.NewGuid().ToString()
+        };
+        
+        var lambert = new Person
+        {
+            Id = 3,
+            Name = "lambert",
+            Surname = "",
+            Cv = "What a prick",
+            UserName = "lambert",
+            IsActive = true,
+            Birthdate = DateTime.Now,
+            NormalizedUserName = "LAMBERT",
+            SecurityStamp = Guid.NewGuid().ToString(),
+        };
+        // Create default passwords for witchers
+        
+        geralt.PasswordHash = new PasswordHasher<Person>().HashPassword(geralt, "GeraltOfRevia123*");
+        vesemir.PasswordHash = new PasswordHasher<Person>().HashPassword(geralt, "OldWolf1*");
+        lambert.PasswordHash = new PasswordHasher<Person>().HashPassword(geralt, "12HandsomeLamb*");
+        
         var odolan = new Contractor() { Id = 1, Name = "Odolan", Surname = "White" };
+        
         var noonWraithContract = new Contract()
         {
             Id = 1,
@@ -38,13 +71,9 @@ public static class DbInitializer
             Location = "White Orchard",
             Person = null
         };
-
-        var userManager = new Role() { Id = 1, Name = "UserManager" };
-
         
-       
         modelBuilder.Entity<Person>()
-            .HasData(geralt);
+            .HasData(geralt, vesemir, lambert);
 
         modelBuilder.Entity<Contractor>()
             .HasData(odolan);
@@ -52,15 +81,24 @@ public static class DbInitializer
         modelBuilder.Entity<Contract>()
             .HasData(noonWraithContract);
 
-        modelBuilder.Entity<Role>()
-            .HasData(userManager);
-
-        modelBuilder.Entity<UserRole>()
-            .HasData(new UserRole()
-            {
-                RoleId = userManager.Id,
-                UserId = geralt.Id
-            });
+        modelBuilder.Entity<Role>().HasData
+        (
+            new Role() { Id = 1, Name = RoleNames.Admin, NormalizedName = RoleNames.Admin.ToUpper(CultureInfo.InvariantCulture)},
+            new Role() { Id = 2, Name = RoleNames.UserManager, NormalizedName = RoleNames.UserManager.ToUpper(CultureInfo.InvariantCulture)},
+            new Role() { Id = 3, Name = RoleNames.Witcher, NormalizedName = RoleNames.Witcher.ToUpper(CultureInfo.InvariantCulture)},
+            new Role() { Id = 4, Name = RoleNames.ContractManager, NormalizedName = RoleNames.ContractManager.ToUpper(CultureInfo.InvariantCulture)}
+        );
+        
+        modelBuilder.Entity<UserRole>().HasData
+        (
+            new UserRole() { RoleId = 1, UserId = geralt.Id },
+            new UserRole() { RoleId = 2, UserId = geralt.Id },
+            new UserRole() { RoleId = 3, UserId = geralt.Id },
+            new UserRole() { RoleId = 4, UserId = geralt.Id },
+            new UserRole() { RoleId = 3, UserId = vesemir.Id },
+            new UserRole() { RoleId = 4, UserId = vesemir.Id },
+            new UserRole() { RoleId = 3, UserId = lambert.Id }
+        );
         
         modelBuilder.Entity<ContractRequest>()
             .HasData(new ContractRequest
