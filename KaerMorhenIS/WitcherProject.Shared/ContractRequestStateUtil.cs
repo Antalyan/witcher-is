282dcb136f7e25ac88cc3c087cd4d772eb31e181
peter-sipos <peter.siipos@gmail.com>
2022-12-13 13:13:44 +0100

    Edited CR states, seperated the detail CR view into separate component.

--- KaerMorhenIS/WitcherProject.Shared/ContractRequestStateUtil.cs
+++ KaerMorhenIS/WitcherProject.Shared/ContractRequestStateUtil.cs
@@ -4,12 +4,6 @@ namespace WitcherProject.Shared;
 
 public static class ContractRequestStateUtil
 {
-    public static IEnumerable<ContractRequestState> GetUserSettableStates()
-    {
-        // TODO: think through the actuall possible user settable states
-        return new List<ContractRequestState> { ContractRequestState.Accepted, ContractRequestState.Approved, ContractRequestState.Declined };
-    }
-    
     public static IEnumerable<ContractRequestState> GetAllStates()
     {
         return Enum.GetValues(typeof(ContractRequestState)).Cast<ContractRequestState>();
