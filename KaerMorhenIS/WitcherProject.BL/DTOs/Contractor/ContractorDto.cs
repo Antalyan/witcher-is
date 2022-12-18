a1f104f01a13f61139a99c25452d39c753ae480f
Michael Koudela <xkoudel2@fi.muni.cz>
2022-12-18 13:08:09 +0100

    Refactoring

--- KaerMorhenIS/WitcherProject.BL/DTOs/Contractor/ContractorDto.cs
+++ KaerMorhenIS/WitcherProject.BL/DTOs/Contractor/ContractorDto.cs
@@ -7,9 +7,7 @@ public class ContractorDto
     public string Name { get; set; }
 
     public string? Surname { get; set; }
-
-    // public virtual List<Contract> Contracts { get; set; } TODO: add contractDto list or id list or nothing?
-
+    
     protected bool Equals(ContractorDto other)
     {
         return Id == other.Id && Name == other.Name && Surname == other.Surname;
