903a897f6133df993b256ce6c56209611d7fee50
Michael Koudela <xkoudel2@fi.muni.cz>
2025-08-20 02:04:11 +0200

    Support multiformat cs

--- KaerMorhenIS/WitcherProject.Infrastructure.EFCore/ServiceCollectionExtensions.cs
+++ KaerMorhenIS/WitcherProject.Infrastructure.EFCore/ServiceCollectionExtensions.cs
@@ -1,26 +1,48 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Npgsql;
 using WitcherProject.DAL;
 
 namespace WitcherProject.Infrastructure.EFCore;
 
 public static class ServiceCollectionExtensions
 {
+    private static string BuildPostgresqlConnectionString(string conn)
+    {
+        if (!conn.StartsWith("postgres://", StringComparison.OrdinalIgnoreCase) &&
+            !conn.StartsWith("postgresql://", StringComparison.OrdinalIgnoreCase)) return conn;
+        var uri = new Uri(conn);
+        var userInfo = uri.UserInfo.Split(':');
+
+        var builder = new NpgsqlConnectionStringBuilder
+        {
+            Host = uri.Host,
+            Port = uri.Port,
+            Database = uri.AbsolutePath.TrimStart('/'),
+            Username = userInfo[0],
+            Password = userInfo[1],
+            SslMode = SslMode.Require,
+            Pooling = true,
+        };
+        return builder.ConnectionString;
+
+    }
+    
     public static IServiceCollection ConfigureDatabase(this IServiceCollection services, IConfiguration config)
     {
         var usedDb = config.GetSection("ActiveDb").Value;
         if (usedDb == "Postgresql")
         {
-            var connectionString = config.GetConnectionString("PostgresqlMain");
+            var connectionString = BuildPostgresqlConnectionString(config.GetConnectionString("PostgresqlMain"));
             services.AddDbContextFactory<KaerMorhenDBContext>(
-                options => options.UseNpgsql(connectionString).EnableSensitiveDataLogging(), ServiceLifetime.Transient);
+                options => options.UseNpgsql(connectionString).EnableSensitiveDataLogging(), ServiceLifetime.Scoped);
             AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
         } else if (usedDb == "SqlServer")
         {
             var connectionString = config.GetConnectionString("SqlServerMain");
             services.AddDbContextFactory<KaerMorhenDBContext>(
-                options => options.UseSqlServer(connectionString).EnableSensitiveDataLogging(), ServiceLifetime.Transient);
+                options => options.UseSqlServer(connectionString).EnableSensitiveDataLogging(), ServiceLifetime.Scoped);
         }
         else
         {
